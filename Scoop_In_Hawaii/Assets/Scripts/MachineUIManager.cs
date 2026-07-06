404a7cea9481fa2f03616f739dec274078c708d9
gpdms06 <gpdms060416@gmail.com>
2026-07-06 19:55:08 +0900

    아이스크림 배너 추가

--- Scoop_In_Hawaii/Assets/Scripts/MachineUIManager.cs
+++ Scoop_In_Hawaii/Assets/Scripts/MachineUIManager.cs
@@ -7,6 +7,8 @@ public class MachineUIManager : MonoBehaviour
     public GameObject conePanel;
     public GameObject barPanel;
     public GameObject machinePanel;
+    public GameObject chocolatePanel;
+    public GameObject VanillaPanel;
 
     public TMP_Text titleText;
 
@@ -18,6 +20,22 @@ public class MachineUIManager : MonoBehaviour
         conePanel.SetActive(true);
 
         titleText.text = "콘 아이스크림 제작";
+    }
+
+    public void OpenChocolate()
+    {
+
+        chocolatePanel.SetActive(true);
+
+        titleText.text = "초콜릿 스쿱";
+    }
+
+    public void OpenVanilla()
+    {
+
+        VanillaPanel.SetActive(true);
+
+        titleText.text = "바닐라 스쿱";
     }
 
     public void OpenBarMachine()
@@ -35,5 +53,6 @@ public class MachineUIManager : MonoBehaviour
         conePanel.SetActive(false);
         barPanel.SetActive(false);
         machinePanel.SetActive(false);
+        chocolatePanel.SetActive(false);
     }
 }
