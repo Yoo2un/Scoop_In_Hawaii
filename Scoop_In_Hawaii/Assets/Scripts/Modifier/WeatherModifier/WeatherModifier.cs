1444be50f2a868b5e7dce2b903bc42570e89499d
yoon <105162326+Yoo2un@users.noreply.github.com>
2026-08-02 23:30:37 +0900

    feat: 폭염 시각 효과 추가
    - 폭염 전용 Volume 추가
    - Vignette 기반 화면 연출 적용
    - Volume Weight로 효과 활성화/비활성화 구현

--- Scoop_In_Hawaii/Assets/Scripts/Modifier/WeatherModifier/WeatherModifier.cs
+++ Scoop_In_Hawaii/Assets/Scripts/Modifier/WeatherModifier/WeatherModifier.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class WeatherModifier : MonoBehaviour
 {
@@ -7,10 +8,12 @@ public class WeatherModifier : MonoBehaviour
 
     public WeatherType CurrentWeather { get; private set; } = WeatherType.None;
 
+    public Volume heatWaveVolume;
+
     [SerializeField] private GameObject rainEffect;
 
     private Coroutine rainCoroutine;
-
+    private Coroutine heatwaveCoroutine;
     private void Awake()
     {
         if (Instance == null)
@@ -72,7 +75,28 @@ public class WeatherModifier : MonoBehaviour
 
     private void StartHeatWave()
     {
-        // ³ªÁß¿¡ ±¸Çö
-        Debug.Log("Æø¿° ¹ß»ý!");
+        if (rainCoroutine != null)
+        {
+            StopCoroutine(heatwaveCoroutine);
+        }
+
+        heatwaveCoroutine = StartCoroutine(HeatWaveRoutine());
+
+        Debug.Log("Æø¿°ÀÔ´Ï´Ù.");
+    }
+
+    private IEnumerator HeatWaveRoutine()
+    {
+        CustomerManager.Instance.SetClientSpawnDelay(5f, 8f);
+
+        heatWaveVolume.weight = 1f;
+
+        yield return new WaitForSeconds(60f);
+
+        CustomerManager.Instance.ResetClientSpawnDelay();
+
+        heatWaveVolume.weight = 0f;
+
+        heatwaveCoroutine = null;
     }
 }
