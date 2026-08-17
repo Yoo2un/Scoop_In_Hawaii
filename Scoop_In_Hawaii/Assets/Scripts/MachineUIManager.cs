a6c854fb20f66d23968ca02bdfd06aa1d63136b6
gpdms06 <gpdms060416@gmail.com>
2026-08-17 20:44:35 +0900

    콘 가이드라인(위치변경필요)

--- Scoop_In_Hawaii/Assets/Scripts/MachineUIManager.cs
+++ Scoop_In_Hawaii/Assets/Scripts/MachineUIManager.cs
@@ -7,6 +7,8 @@ public class MachineUIManager : MonoBehaviour
     public GameObject conePanel;
     public GameObject barPanel;
     public GameObject machinePanel;
+    public GameObject chocolatePanel;
+    public GameObject vanillaPanel;
 
     public TMP_Text titleText;
     [SerializeField] private GameObject RepairPanel;
@@ -30,6 +32,22 @@ public class MachineUIManager : MonoBehaviour
         conePanel.SetActive(true);
 
         titleText.text = "콘 아이스크림 제작";
+    }
+
+    public void OpenChocolate()
+    {
+
+        chocolatePanel.SetActive(true);
+
+        titleText.text = "초콜릿 스쿱";
+    }
+
+    public void OpenVanilla()
+    {
+
+        vanillaPanel.SetActive(true);
+
+        titleText.text = "바닐라 스쿱";
     }
 
     public void OpenBarMachine()
@@ -67,6 +85,8 @@ public class MachineUIManager : MonoBehaviour
         conePanel.SetActive(false);
         barPanel.SetActive(false);
         machinePanel.SetActive(false);
+        chocolatePanel.SetActive(false);
+        vanillaPanel.SetActive(false);
     }
 
 }
