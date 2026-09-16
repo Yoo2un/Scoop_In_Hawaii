c9435de48a530794a69b72f20cd039e6b3f3fae2
yoon <105162326+Yoo2un@users.noreply.github.com>
2026-09-16 22:20:08 +0900

    panel가이드라인 수정

--- Scoop_In_Hawaii/Assets/Scripts/MachineUIManager.cs
+++ Scoop_In_Hawaii/Assets/Scripts/MachineUIManager.cs
@@ -48,15 +48,14 @@ public class MachineUIManager : MonoBehaviour
     public void OpenChocolate()
     {
 
-        //chocolatePanel.SetActive(true);
+        chocolatePanel.SetActive(true);
 
         titleText.text = "초콜릿 스쿱";
     }
 
     public void OpenVanilla()
     {
-
-        //vanillaPanel.SetActive(true);
+        vanillaPanel.SetActive(true);
 
         titleText.text = "바닐라 스쿱";
     }
